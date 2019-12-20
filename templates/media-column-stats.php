8b7b057a50dca7af2df276903d29ea427a279c07
Harun Bašić <harun@bleech.de>
2019-12-20 11:16:35 +0100

    reafactor: add wrapper for media columns stats

--- templates/media-column-stats.php
+++ templates/media-column-stats.php
@@ -33,53 +33,59 @@ $summary = $stats['stats']; ?>
 
 			<?php if ( $summary['total'] ) : ?>
 
-				<?php
-					/* translators: %1$s percentage %2$s total */
-					echo esc_html( sprintf( __( 'Saved %1$s (%2$s)', 'kraken-io' ), $summary['percentage'], $summary['total'] ) );
-				?>
+				<div class="kraken-stas-savings">
 
-				<div class="kraken-stats-action">
-					<a href="#details" class="kraken-stats-action-show-details"><?php esc_html_e( 'Show Details', 'kraken-io' ); ?></a>
+					<?php
+						/* translators: %1$s percentage %2$s total */
+						echo esc_html( sprintf( __( 'Saved %1$s (%2$s)', 'kraken-io' ), $summary['percentage'], $summary['total'] ) );
+					?>
 
-					<div class="kraken-stats-action-popup">
+					<div class="kraken-stats-action">
+						<a href="#details" class="kraken-stats-action-show-details"><?php esc_html_e( 'Show Details', 'kraken-io' ); ?></a>
 
-						<button type="button" class="kraken-stats-action-popup-close dashicons dashicons-no-alt"></button>
+						<div class="kraken-stats-action-popup">
 
-						<?php if ( $summary['is_main_image_optimized'] ) : ?>
+							<button type="button" class="kraken-stats-action-popup-close dashicons dashicons-no-alt"></button>
+
+							<?php if ( $summary['is_main_image_optimized'] ) : ?>
+								<p>
+									<?php
+										/* translators: %1$s bytes %2$s percentage */
+										echo esc_html( sprintf( __( 'Main image savings: %1$s (%2$s saved)', 'kraken-io' ), $summary['main_image_stats']['saved_bytes'], $summary['main_image_stats']['savings_percentage'] ) );
+									?>
+								</p>
+							<?php endif; ?>
+
+							<?php if ( $summary['is_thumbs_optimized'] ) : ?>
+								<p>
+									<?php
+										/* translators: %1$s count %2$s savings %3$s percentage */
+										echo esc_html( sprintf( __( 'Savings on %1$s thumbnails: %2$s (%3$s saved)', 'kraken-io' ), $summary['thumbs_count'], $summary['thumbs_stats']['total_savings'], $summary['thumbs_stats']['savings_percentage'] ) );
+									?>
+								</p>
+							<?php endif; ?>
+
 							<p>
 								<?php
-									/* translators: %1$s bytes %2$s percentage */
-									echo esc_html( sprintf( __( 'Main image savings: %1$s (%2$s saved)', 'kraken-io' ), $summary['main_image_stats']['saved_bytes'], $summary['main_image_stats']['savings_percentage'] ) );
+									/* translators: %s optimization_mode */
+									echo esc_html( sprintf( __( 'Optimization mode: %s', 'kraken-io' ), $summary['optimization_mode'] ) );
 								?>
 							</p>
-						<?php endif; ?>
-
-						<?php if ( $summary['is_thumbs_optimized'] ) : ?>
-							<p>
-								<?php
-									/* translators: %1$s count %2$s savings %3$s percentage */
-									echo esc_html( sprintf( __( 'Savings on %1$s thumbnails: %2$s (%3$s saved)', 'kraken-io' ), $summary['thumbs_count'], $summary['thumbs_stats']['total_savings'], $summary['thumbs_stats']['savings_percentage'] ) );
-								?>
-							</p>
-						<?php endif; ?>
-
-						<p>
-							<?php
-								/* translators: %s optimization_mode */
-								echo esc_html( sprintf( __( 'Optimization mode: %s', 'kraken-io' ), $summary['optimization_mode'] ) );
-							?>
-						</p>
+						</div>
 					</div>
+
+					<?php if ( $stats['show_reset'] ) : ?>
+						<div class="kraken-stats-action">
+							<a href="#reset" class="kraken-stats-action-reset-image" data-id="<?php echo esc_attr( $stats['id'] ); ?>"><?php esc_html_e( 'Reset Image', 'kraken-io' ); ?><span class="spinner"></span></a>
+						</div>
+					<?php endif; ?>
+
 				</div>
 
-				<?php if ( $stats['show_reset'] ) : ?>
-					<div class="kraken-stats-action">
-						<a href="#reset" class="kraken-stats-action-reset-image" data-id="<?php echo esc_attr( $stats['id'] ); ?>"><?php esc_html_e( 'Reset Image', 'kraken-io' ); ?><span class="spinner"></span></a>
-					</div>
-				<?php endif; ?>
-
 			<?php else : ?>
-				<?php esc_html_e( 'No savings', 'kraken-io' ); ?>
+				<div class="kraken-stas-no-savings">
+					<?php esc_html_e( 'No savings', 'kraken-io' ); ?>
+				</div>
 			<?php endif; ?>
 
 		<?php else : ?>
@@ -97,7 +103,7 @@ $summary = $stats['stats']; ?>
 			</button>
 
 			<?php if ( ! $stats['has_savings'] ) : ?>
-				<div class="kraken-no-savings">
+				<div class="kraken-stats-no-savings">
 					<?php
 						/* translators: %s optimization type */
 						echo esc_html( sprintf( __( 'No savings found for type %s', 'kraken-io' ), $stats['type'] ) );
@@ -106,10 +112,12 @@ $summary = $stats['stats']; ?>
 			<?php endif; ?>
 
 			<?php if ( $stats['has_error'] ) : ?>
-				<?php
-					/* translators: %s error */
-					echo esc_html( sprintf( __( 'Failed with errors %s', 'kraken-io' ), $stats['has_error'] ) );
-				?>
+				<div class="kraken-stats-failed-optimize">
+					<?php
+						/* translators: %s error */
+						echo esc_html( sprintf( __( 'Failed with errors %s', 'kraken-io' ), $stats['has_error'] ) );
+					?>
+				</div>
 			<?php endif; ?>
 
 
