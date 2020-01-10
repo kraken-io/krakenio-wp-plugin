720f4e298118ed7a39b7a822eb946a26a7d43cac
Harun Bašić <harun@bleech.de>
2020-01-10 17:12:30 +0100

    refactor: incompatibility with wp_remote_post and kraken upload endpoint

--- includes/class-kraken-io-api.php
+++ includes/class-kraken-io-api.php
@@ -10,8 +10,10 @@ defined( 'ABSPATH' ) || exit;
 
 class Kraken_IO_API {
 
-	protected $auth     = [];
-	protected $endpoint = 'https://api.kraken.io/';
+	protected $auth        = [];
+	protected $endpoint    = 'https://api.kraken.io/';
+	protected $api_version = 'v1';
+	protected $timeout     = 30;
 
 	public function __construct( $key = '', $secret = '' ) {
 		$this->auth = [
@@ -22,44 +24,77 @@ class Kraken_IO_API {
 		];
 	}
 
-	private function request( $data, $url ) {
+	private function return_error( $data = [] ) {
+		return wp_parse_args( [ 'success' => false ], $data );
+	}
+
+	private function request( $data, $url, $is_json = true ) {
 
 		if ( empty( $data ) ) {
-			return false;
+			return $this->return_error(
+				[
+					'type' => 'empty_data',
+				]
+			);
 		}
 
-		$data = [
-			'body' => wp_json_encode( $data ),
-		];
+		// @codingStandardsIgnoreStart
+		$curl = curl_init();
 
-		$response = wp_remote_post( $url, $data );
-
-		if ( is_wp_error( $response ) ) {
-			return false;
+		if ( $is_json ) {
+			curl_setopt(
+				$curl,
+				CURLOPT_HTTPHEADER,
+				[
+					'Content-Type: application/json',
+				]
+			);
 		}
 
-		$http_code = $response['response']['code'];
-		$result    = json_decode( $response['body'], true );
+		curl_setopt( $curl, CURLOPT_URL, $url );
+
+		// Force continue-100 from server
+		curl_setopt( $curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36' );
+		curl_setopt( $curl, CURLOPT_POST, 1 );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt( $curl, CURLOPT_POSTFIELDS, $data );
+		curl_setopt( $curl, CURLOPT_FAILONERROR, 0 );
+		curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, 1 );
+		curl_setopt( $curl, CURLOPT_TIMEOUT, $this->timeout );
+
+		$response  = json_decode( curl_exec( $curl ), true );
+		$http_code = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
 
 		if ( 200 !== $http_code ) {
-			return false;
+			return $this->return_error(
+				[
+					'type' => 'wrong_status_code',
+				]
+			);
 		}
 
-		if ( isset( $result['errors'] ) ) {
-			return false;
+		if ( null === $response ) {
+			return $this->return_error(
+				[
+					'type' => 'empty_response',
+				]
+			);
 		}
 
-		return $result;
+		curl_close( $curl );
+		// @codingStandardsIgnoreEnd
+
+		return $response;
 	}
 
 	public function url( $opts = [] ) {
-		$data = array_merge( $this->auth, $opts );
-		return $this->request( $data, $this->endpoint . '/v1/url' );
+		$data = wp_json_encode( array_merge( $this->auth, $opts ) );
+		return $this->request( $data, $this->endpoint . $this->api_version . '/url' );
 	}
 
 	public function upload( $opts = [] ) {
 		if ( ! isset( $opts['file'] ) ) {
-			wp_send_json_error(
+			return $this->return_error(
 				[
 					'type' => 'file_not_provided',
 				]
@@ -67,24 +102,30 @@ class Kraken_IO_API {
 		}
 
 		if ( ! file_exists( $opts['file'] ) ) {
-			wp_send_json_error(
+			return $this->return_error(
 				[
 					'type' => 'file_not_found',
 				]
 			);
 		}
 
-		$file = '@' . $opts['file'];
+		if ( class_exists( 'CURLFile' ) ) {
+			$file = new CURLFile( $opts['file'] );
+		} else {
+			$file = '@' . $opts['file'];
+		}
+
+		unset( $opts['file'] );
 
 		$data = [
 			'file' => $file,
-			'data' => array_merge( $this->auth, $opts ),
+			'data' => wp_json_encode( array_merge( $this->auth, $opts ) ),
 		];
 
-		return $this->request( $data, $this->endpoint . 'v1/upload' );
+		return $this->request( $data, $this->endpoint . $this->api_version . '/upload', false );
 	}
 
 	public function status() {
-		return $this->request( $this->auth, $this->endpoint . 'user_status' );
+		return $this->request( wp_json_encode( $this->auth ), $this->endpoint . 'user_status' );
 	}
 }
