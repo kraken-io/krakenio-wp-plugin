d74c6f8ce1cf9ff1136a31db3ee4bc5ec7dd22b3
Harun Bašić <harun@bleech.de>
2020-01-10 03:24:33 +0100

    refactor: split optimization code

--- includes/class-kraken-io-optimization.php
+++ includes/class-kraken-io-optimization.php
@@ -21,7 +21,7 @@ class Kraken_IO_Optimization {
 
 		if ( $this->options['auto_optimize'] ) {
 			add_action( 'add_attachment', [ $this, 'optimize_image_on_upload' ] );
-			add_filter( 'wp_generate_attachment_metadata', [ $this, 'optimize_thumbnails' ] );
+			add_filter( 'wp_generate_attachment_metadata', [ $this, 'optimize_thumbnails_on_resize' ], 10, 2 );
 		}
 	}
 
@@ -62,49 +62,42 @@ class Kraken_IO_Optimization {
 	}
 
 	/**
-	 * Converts an deserialized API result array into an array
-	 * which this plugin will consume
+	 * Format optimization response for meta
 	 *
 	 * @since  2.7
-	 * @access public
-	 * @param  array $result
-	 * @param  int $image_id
-	 * @return array $result
+	 * @access private
+	 * @param  array $response
+	 * @param  int $id
+	 * @return array $response
 	 */
-	public function get_result_arr( $result, $image_id ) {
-		$rv = [];
+	private function format_optimization_response( $response, $id ) {
 
-		$rv['original_size'] = $result['original_size'];
-		$rv['kraked_size']   = $result['kraked_size'];
-		$rv['saved_bytes']   = $result['saved_bytes'];
+		$savings_percentage          = $response['saved_bytes'] / $response['original_size'] * 100;
+		$response['savings_percent'] = round( $savings_percentage, 2 ) . '%';
 
-		$savings_percentage = $result['saved_bytes'] / $result['original_size'] * 100;
-
-		$rv['savings_percent'] = round( $savings_percentage, 2 ) . '%';
-		$rv['type']            = $result['type'];
-
-		if ( ! empty( $result['kraked_width'] ) && ! empty( $result['kraked_height'] ) ) {
-			$rv['kraked_width']  = $result['kraked_width'];
-			$rv['kraked_height'] = $result['kraked_height'];
-		}
-
-		$rv['success'] = $result['success'];
-		$rv['meta']    = wp_get_attachment_metadata( $image_id );
-
-		return $rv;
+		return $response;
 	}
+
 
 	/**
 	 * Replace image with optimized.
 	 *
 	 * @since  2.7
-	 * @access public
-	 * @param  string $image_path
-	 * @param  string $kraked_url
+	 * @access private
+	 * @param  string $path
+	 * @param  string $url
 	 * @return bool
 	 */
-	public function replace_image( $image_path, $kraked_url ) {
+	private function replace_image( $path, $url ) {
 
+		$optimized_image_contents = file_get_contents( $url );
+		$replaced_image           = false;
+
+		if ( $optimized_image_contents ) {
+			$replaced_image = file_put_contents( $path, $optimized_image_contents );
+		}
+
+		return $replaced_image !== false;
 	}
 
 	public function get_preserve_meta_options( $options ) {
@@ -135,12 +128,12 @@ class Kraken_IO_Optimization {
 	}
 
 	/**
-	 * Optimize image.
+	 * Get optimized image from api.
 	 *
 	 * @since  2.7
-	 * @access public
+	 * @access private
 	 */
-	public function optimize_image( $image_path, $type, $resize = false ) {
+	private function get_optimized_image( $image_path, $type ) {
 		$settings = $this->options;
 
 		if ( ! empty( $type ) ) {
@@ -170,9 +163,11 @@ class Kraken_IO_Optimization {
 			$params['auto_orient'] = true;
 		}
 
-		if ( $resize ) {
+		if ( ! empty( $settings['resize_width'] ) || ! empty( $settings['resize_height'] ) ) {
+
 			$width  = (int) $settings['resize_width'];
 			$height = (int) $settings['resize_height'];
+
 			if ( $width && $height ) {
 				$params['resize'] = [
 					'strategy' => 'auto',
@@ -196,19 +191,142 @@ class Kraken_IO_Optimization {
 			$params['quality'] = (int) $settings['jpeg_quality'];
 		}
 
-		$response = kraken_io()->api->upload( $params );
+		$response         = kraken_io()->api->upload( $params );
+		$response['type'] = ! empty( $type ) ? $type : $settings['api_lossy'];
 
 		return $response;
 	}
 
 	/**
-	 * Optimize tumbnails.
+	 * Optimize single image.
+	 *
+	 * @since  2.7
+	 * @access private
+	 * @param  string $path
+	 * @param  string $type
+	 * @return bool
+	 */
+	private function optimize_single_image( $path, $type = null ) {
+
+		$optimized_image = $this->get_optimized_image( $path, $type );
+
+		if ( $optimized_image['success'] ) {
+
+			if ( ! $this->replace_image( $path, $optimized_image['kraked_url'] ) ) {
+				return false;
+			}
+
+			return $optimized_image;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Optimize main image.
 	 *
 	 * @since  2.7
 	 * @access public
 	 */
-	public function optimize_thumbnails() {
+	public function optimize_main_image( $id, $type = null ) {
 
+		$path = get_attached_file( $id );
+
+		// the image doesn't exist
+		if ( ! $path ) {
+			return false;
+		}
+
+		$backup_path = $path . '_kraken_' . md5( $path );
+		$data        = [];
+
+		if ( copy( $path, $backup_path ) ) {
+			$data['optimized_backup_file'] = $backup_path;
+		}
+
+		$optimized_image = $this->optimize_single_image( $path, $type );
+
+		if ( $optimized_image ) {
+
+			$data = wp_parse_args( $this->format_optimization_response( $optimized_image, $id ), $data );
+			update_post_meta( $id, '_kraken_size', $data );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Optimize thumbnails.
+	 *
+	 * @since  2.7
+	 * @access public
+	 */
+	public function optimize_thumbnails( $id, $type = null ) {
+
+		$kraked_thumbs = get_post_meta( $id, '_kraked_thumbs', true );
+
+		if ( $kraked_thumbs ) {
+			return false;
+		}
+
+		$metadata = wp_get_attachment_metadata( $id );
+		$sizes = $this->get_image_sizes_to_optimize();
+		$thumb_data = [];
+
+		$upload_dir = wp_upload_dir();
+		$path_parts = pathinfo( $metadata['file'] );
+
+		// e.g. 04/02, for use in getting correct path or URL
+		$upload_subdir = $path_parts['dirname'];
+
+		// all the way up to /uploads
+		$upload_base_path = $upload_dir['basedir'];
+		$upload_full_path = $upload_base_path . '/' . $upload_subdir;
+
+		foreach ( $metadata['sizes'] as $key => $size ) {
+
+			if ( in_array( $key, $sizes, true ) ) {
+				$path            = $upload_full_path . '/' . $size['file'];
+				$optimized_image = $this->optimize_single_image( $path, $type );
+
+				if ( $optimized_image ) {
+					$thumb_data[] = [
+						'thumb'         => $key,
+						'file'          => $size['file'],
+						'original_size' => $optimized_image['original_size'],
+						'kraked_size'   => $optimized_image['kraked_size'],
+						'type'          => $optimized_image['type'],
+					];
+				}
+			}
+		}
+
+		if ( $thumb_data ) {
+			update_post_meta( $id, '_kraked_thumbs', $thumb_data, false );
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Optimize image.
+	 *
+	 * @since  2.7
+	 * @access public
+	 */
+	public function optimize_image( $id, $type = null ) {
+
+		$optimized_main_image = $this->optimize_main_image( $id, $type );
+		$optimized_thumbnails = $this->optimize_thumbnails( $id, $type );
+
+		if ( $optimized_main_image || $optimized_thumbnails ) {
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
@@ -216,77 +334,30 @@ class Kraken_IO_Optimization {
 	 *
 	 * @since  2.7
 	 * @access public
-	 * @param  int $image_id
+	 * @param  int $id
 	 */
-	public function optimize_image_on_upload( $image_id ) {
+	public function optimize_image_on_upload( $id ) {
 
 		if ( empty( $this->options['optimize_main_image'] ) ) {
 			return false;
 		}
 
-		$settings = $this->options;
-		$type     = $settings['api_lossy'];
-
-		if ( ! wp_attachment_is_image( $image_id ) ) {
+		if ( ! wp_attachment_is_image( $id ) ) {
 			return false;
 		}
 
-		$image_path        = get_attached_file( $image_id );
-		$image_backup_path = $image_path . '_kraken_' . md5( $image_path );
-		$backup_created    = false;
+		$this->optimize_main_image( $id );
+	}
 
-		if ( copy( $image_path, $image_backup_path ) ) {
-			$backup_created = true;
-		}
-
-		$resize = false;
-
-		if ( ! empty( $settings['resize_width'] ) || ! empty( $settings['resize_height'] ) ) {
-			$resize = true;
-		}
-
-		// optimize backup image
-		if ( $backup_created ) {
-			$api_result = $this->optimize_image( $image_backup_path, $type, $resize );
-		} else {
-			$api_result = $this->optimize_image( $image_path, $type, $resize );
-		}
-
-		$data = [];
-
-		if ( ! empty( $api_result ) && ! empty( $api_result['success'] ) ) {
-			$data = $this->get_result_arr( $api_result, $image_id );
-
-			if ( $backup_created ) {
-				$data['optimized_backup_file'] = $image_backup_path;
-
-				if ( $data['saved_bytes'] > 0 ) {
-					if ( ! $this->replace_image( $image_backup_path, $api_result['kraked_url'] ) ) {
-						$data['error'] = 'replace_image';
-					}
-				}
-			} else {
-				if ( $data['saved_bytes'] > 0 ) {
-					if ( ! $this->replace_image( $image_path, $api_result['kraked_url'] ) ) {
-						$data['error'] = 'replace_image';
-					}
-				}
-			}
-
-			update_post_meta( $image_id, '_kraken_size', $data );
-
-		} else {
-			// error or no optimization
-			if ( file_exists( $image_path ) ) {
-
-				$data['original_size'] = filesize( $image_path );
-				$data['error']         = $api_result['message'];
-				$data['type']          = $api_result['type'];
-
-				update_post_meta( $image_id, '_kraken_size', $data );
-			}
-		}
-
+	/**
+	 * Optimize thumbnails when they are generated.
+	 *
+	 * @since  2.7
+	 * @access public
+	 */
+	public function optimize_thumbnails_on_resize( $metadata, $id ) {
+		$this->optimize_thumbnails( $id );
+		return $metadata;
 	}
 
 	/**
@@ -296,6 +367,14 @@ class Kraken_IO_Optimization {
 	 * @access public
 	 */
 	public function get_image_sizes_to_optimize() {
+		$sizes = array();
 
+		foreach( $this->options as $key => $value ) {
+			if ( strpos( $key, 'include_size' ) === 0 && !empty( $value ) ) {
+				$sizes[] = str_replace( 'include_size_', '', $key );
+			}
+		}
+
+		return $sizes;
 	}
 }
